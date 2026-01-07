2c35fad5f09adf3335eb59cff3e3af89b5685fc1
Sasha Marie te Rehorst <sasha.marieterehorst@gmail.com>
2026-01-07 18:32:16 +0100

    Algue: Refonte graphique
    - Ajouts d'une animation et d'un meilleur look
    
    Signed-off-by: Sasha Marie te Rehorst <sasha.marieterehorst@gmail.com>

--- src/algue.cpp
+++ src/algue.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "algue.hpp"
 #include "monde.hpp"
 
@@ -9,8 +10,21 @@ void Algue::update(float dt, Monde& monde) {
 }
 
 void Algue::dessiner(sf::RenderTarget& cible) const {
-    sf::CircleShape cercle(rayon);
-    cercle.setFillColor(sf::Color::Green);
-    cercle.setPosition(position - sf::Vector2f(rayon, rayon));
-    cible.draw(cercle);
+    static sf::Clock horloge;
+    float temps = horloge.getElapsedTime().asSeconds();
+    const int nbSegments = 10;
+    const float epaisseurBase = 4.f;
+    const float hauteurSegment = 4.f;
+    
+    for (int i = 0; i < nbSegments; ++i) {
+        float ratio = static_cast<float>(i) / nbSegments;
+        float taille = epaisseurBase * (1.0f - ratio * 0.7f);
+        float oscillation = std::sin(temps * 1.5f + i * 0.5f) * (i * 0.6f);
+        
+        sf::CircleShape segment(taille);
+        segment.setOrigin(sf::Vector2f(taille, taille));
+        segment.setFillColor(sf::Color(34 + i*5, 120 + i*8, 50 + i*2, 255 - i*15));
+        segment.setPosition(position + sf::Vector2f(oscillation, -i * hauteurSegment));
+        cible.draw(segment);
+    }
 }
