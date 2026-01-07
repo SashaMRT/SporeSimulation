4af9dd93ea359efcae68e8959239813aab57176a
Sasha Marie te Rehorst <sasha.marieterehorst@gmail.com>
2026-01-07 22:08:38 +0100

    Systeme: Finalisation des herbivores et carnivores
    - Création graphique définitives des herbivores et carnivores
    
    Signed-off-by: Sasha Marie te Rehorst <sasha.marieterehorst@gmail.com>

--- src/carnivore.cpp
+++ src/carnivore.cpp
@@ -2,6 +2,7 @@
 #include "carnivore.hpp"
 #include "monde.hpp"
 #include <cmath>
+#include <SFML/System/Angle.hpp>
 
 Carnivore::Carnivore(sf::Vector2f pos) 
     : Entite(pos, TypeEntite::CARNIVORE, 20.f, 120.f) {
@@ -23,6 +24,10 @@ void Carnivore::update(float dt, Monde& monde) {
             energie += 50.f;
             cible->tuer();
         }
+
+        if (vitesse.x != 0 || vitesse.y != 0) {
+            float angle = std::atan2(vitesse.y, vitesse.x) * 180.f / 3.14159f;
+        }
     }
 
     position += vitesse * dt;
@@ -36,9 +41,54 @@ void Carnivore::update(float dt, Monde& monde) {
 }
 
 void Carnivore::dessiner(sf::RenderTarget& cible) const {
-    sf::CircleShape forme(rayon, 3);
-    forme.setFillColor(sf::Color::Red);
-    forme.setOrigin({rayon, rayon});
-    forme.setPosition(position);
-    cible.draw(forme);
+    float angleRad = std::atan2(vitesse.y, vitesse.x);
+    float angleDeg = angleRad * 180.f / 3.14159f;
+    sf::Vector2f dirArriere = {-std::cos(angleRad), -std::sin(angleRad)};
+
+    for(int i = 3; i >= 1; --i) {
+        float tailleQueue = rayon * (0.7f - i * 0.1f);
+        sf::ConvexShape segment(3);
+        segment.setPoint(0, {tailleQueue * 1.5f, 0});
+        segment.setPoint(1, {-tailleQueue, -tailleQueue * 0.7f});
+        segment.setPoint(2, {-tailleQueue, tailleQueue * 0.7f});
+        segment.setPosition(position + dirArriere * (float)i * (rayon * 0.7f));
+        segment.setRotation(sf::degrees(angleDeg + std::sin((position.x + position.y) * 0.1f + i) * 15.f));
+        segment.setFillColor(sf::Color(180 - i*20, 30, 30));
+        segment.setOutlineThickness(1.f); segment.setOutlineColor(sf::Color(100, 0, 0));
+        cible.draw(segment);
+    }
+
+    sf::ConvexShape corps(4);
+    corps.setPoint(0, {rayon * 1.8f, 0}); corps.setPoint(1, {0, -rayon});
+    corps.setPoint(2, {-rayon * 0.8f, 0}); corps.setPoint(3, {0, rayon});
+    corps.setPosition(position);
+    corps.setRotation(sf::degrees(angleDeg));
+    corps.setFillColor(sf::Color(220, 50, 50));
+    corps.setOutlineThickness(2.f); corps.setOutlineColor(sf::Color(150, 20, 20));
+    cible.draw(corps);
+
+    sf::Vector2f off = {std::cos(angleRad), std::sin(angleRad)};
+    sf::CircleShape oeil(rayon * 0.25f);
+    oeil.setOrigin({rayon * 0.25f, rayon * 0.25f});
+    oeil.setPosition(position + off * rayon * 0.5f);
+    oeil.setFillColor(sf::Color::Yellow); oeil.setOutlineThickness(1.f); oeil.setOutlineColor(sf::Color::Black);
+    cible.draw(oeil);
+
+    sf::CircleShape pupille(rayon * 0.1f);
+    pupille.setOrigin({rayon * 0.1f, rayon * 0.1f});
+    pupille.setPosition(oeil.getPosition() + off * rayon * 0.1f);
+    pupille.setFillColor(sf::Color::Black);
+    cible.draw(pupille);
+
+    sf::ConvexShape pointe(3);
+    pointe.setPoint(0, {rayon * 0.4f, 0}); pointe.setPoint(1, {0, -rayon * 0.2f}); pointe.setPoint(2, {0, rayon * 0.2f});
+    pointe.setFillColor(sf::Color(100, 0, 0));
+
+    pointe.setPosition(position + sf::Vector2f(std::cos(angleRad - 1.57f), std::sin(angleRad - 1.57f)) * rayon);
+    pointe.setRotation(sf::degrees(angleDeg - 90));
+    cible.draw(pointe);
+
+    pointe.setPosition(position + sf::Vector2f(std::cos(angleRad + 1.57f), std::sin(angleRad + 1.57f)) * rayon);
+    pointe.setRotation(sf::degrees(angleDeg + 90));
+    cible.draw(pointe);
 }
--- src/herbivore.cpp
+++ src/herbivore.cpp
@@ -34,9 +34,34 @@ void Herbivore::update(float dt, Monde& monde) {
 }
 
 void Herbivore::dessiner(sf::RenderTarget& cible) const {
-    sf::CircleShape forme(rayon);
-    forme.setFillColor(sf::Color::Green);
-    forme.setOrigin({rayon, rayon});
-    forme.setPosition(position);
-    cible.draw(forme);
+    float angleMouv = std::atan2(vitesse.y, vitesse.x);
+
+    for (int i = 0; i < 5; ++i) {
+        float angleBase = angleMouv + 3.14159f + (i - 2.5f) * 0.3f;
+        for (int k = 0; k < 6; ++k) {
+            float angle = angleBase + std::sin((position.x + position.y) * 0.05f + k * 0.5f + i) * 0.2f;
+            float dist = rayon * (0.8f + k * 0.4f);
+            float taille = rayon * 0.25f * (1.f - k / 6.f);
+
+            sf::CircleShape tentacules(taille);
+            tentacules.setOrigin({taille, taille});
+            tentacules.setPosition(position + sf::Vector2f(std::cos(angle), std::sin(angle)) * dist);
+            tentacules.setFillColor(sf::Color(100, 255, 100, 150 - k * 20));
+            cible.draw(tentacules);
+        }
+    }
+
+    sf::CircleShape corps(rayon);
+    corps.setOrigin({rayon, rayon}); 
+    corps.setPosition(position);
+    corps.setFillColor(sf::Color(50, 220, 100, 200));
+    corps.setOutlineThickness(2.f); 
+    corps.setOutlineColor(sf::Color(150, 255, 150));
+    cible.draw(corps);
+
+    sf::CircleShape reflet(rayon * 0.3f);
+    reflet.setOrigin({rayon * 0.3f, rayon * 0.3f});
+    reflet.setPosition(position + sf::Vector2f(-rayon * 0.2f, -rayon * 0.2f));
+    reflet.setFillColor(sf::Color(255, 255, 255, 100));
+    cible.draw(reflet);
 }
